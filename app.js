7dae586a54a0c6b3b3bb5b3fab31f95d3709e8e7
Yara Tercero-Parker <yctercero@users.noreply.github.com>
2016-04-10 20:41:14 -0400

    First commit - uploaded files

---
+++ app.js
@@ -0,0 +1,38 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function($routeProvider) {
+
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+
+});
+
+// SERVICES
+weatherApp.service('cityService', function(){
+	this.city = "New York, NY";
+});
+
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+
+	$scope.$watch('city', function(){
+		cityService.city = $scope.city;
+	});
+}]);
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
+}]);
